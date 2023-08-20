17143964388e072e8f947110be2a2811505042fa
Nikolai Malygin <n.p.malygin@gmail.com>
2023-08-20 16:36:02 +0300

    minor fixes

--- src/test/java/guru/qa/tests/FileParsingTest.java
+++ src/test/java/guru/qa/tests/FileParsingTest.java
@@ -23,62 +23,63 @@ public class FileParsingTest {
 
     @Test
     public void checkZipFile() throws Exception {
-        try (InputStream stream = cl.getResourceAsStream("zip_example.zip");
-             ZipInputStream zis = new ZipInputStream(stream)) {
+        try (InputStream stream = cl.getResourceAsStream("zip_example.zip")) {
+            assert stream != null;
+            try (ZipInputStream zis = new ZipInputStream(stream)) {
 
-            ZipEntry entry;
+                ZipEntry entry;
 
-            while ((entry = zis.getNextEntry()) != null) {
-                String entryName = entry.getName();
-                String ext = Files.getFileExtension(entryName);
+                while ((entry = zis.getNextEntry()) != null) {
+                    String entryName = entry.getName();
+                    String ext = Files.getFileExtension(entryName);
 
 
-                switch (ext) {
+                    switch (ext) {
+                        case ("csv") -> {
+                            try (InputStream str = cl.getResourceAsStream(entryName)) {
+                                assert str != null;
+                                try (Reader reader = new InputStreamReader(str)) {
 
-                    case ("csv"):
-                        try (InputStream str = cl.getResourceAsStream(entryName);
-                             Reader reader = new InputStreamReader(str)) {
+                                    CSVReader csvReader = new CSVReader(reader);
+                                    List<String[]> content = csvReader.readAll();
 
-                            CSVReader csvReader = new CSVReader(reader);
-                            List<String[]> content = csvReader.readAll();
+                                    Assertions.assertThat(content.size()).isEqualTo(3);
 
-                            Assertions.assertThat(content.size()).isEqualTo(3);
+                                    final String[] firstRow = content.get(0);
+                                    final String[] secondRow = content.get(1);
+                                    final String[] thirdRow = content.get(2);
 
-                            final String[] firstRow = content.get(0);
-                            final String[] secondRow = content.get(1);
-                            final String[] thirdRow = content.get(2);
+                                    Assertions.assertThat(firstRow).isEqualTo(new String[]{"тест1", "тест2", "тест123"});
+                                    Assertions.assertThat(secondRow).isEqualTo(new String[]{"это", "зеленый", "тест"});
+                                    Assertions.assertThat(thirdRow).isEqualTo(new String[]{"тест2", "тест3", "тест666"});
 
-                            Assertions.assertThat(firstRow).isEqualTo(new String[]{"тест1", "тест2", "тест123"});
-                            Assertions.assertThat(secondRow).isEqualTo(new String[]{"это", "зеленый", "тест"});
-                            Assertions.assertThat(thirdRow).isEqualTo(new String[]{"тест2", "тест3", "тест666"});
-
-                            break;
+                                }
+                            }
                         }
+                        case ("pdf") -> {
+                            try (InputStream str = cl.getResourceAsStream(entryName)) {
+                                assert str != null;
+                                PDF pdf = new PDF(str);
 
-                    case ("pdf"):
-                        try (InputStream str = cl.getResourceAsStream(entryName)) {
-                            assert str != null;
-                            PDF pdf = new PDF(str);
+                                String content = pdf.text;
 
-                            String content = pdf.text;
+                                Assertions.assertThat(content).contains("Билет на каток");
 
-                            Assertions.assertThat(content).contains("Билет на каток");
-
-                            break;
+                            }
                         }
+                        case ("xlsx") -> {
+                            try (InputStream str = cl.getResourceAsStream(entryName)) {
+                                assert str != null;
+                                XLS xls = new XLS(str);
 
-                    case ("xlsx"):
-                        try (InputStream str = cl.getResourceAsStream(entryName)) {
-                            assert str != null;
-                            XLS xls = new XLS(str);
+                                String testedCell = xls.excel.getSheetAt(2).getRow(2).getCell(1).toString();
 
-                            String testedCell = xls.excel.getSheetAt(2).getRow(2).getCell(1).toString();
-
-                            Assertions.assertThat(testedCell).isEqualTo("Склад");
-                            break;
+                                Assertions.assertThat(testedCell).isEqualTo("Склад");
+                            }
                         }
+                    }
+
                 }
-
             }
         }
     }
@@ -99,7 +100,7 @@ public class FileParsingTest {
     }
 
     @Test
-    public void inprovedCheckJsonFile() throws Exception {
+    public void improvedCheckJsonFile() throws Exception {
         try (InputStream str = cl.getResourceAsStream("json_example.json")) {
 
             ObjectMapper objectMapper = new ObjectMapper();
